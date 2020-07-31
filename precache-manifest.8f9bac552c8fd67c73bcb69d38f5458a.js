bc2222db09238736f3d6ee0173d04d17cdb39172
Newcrop <lmh0870@naver.com>
2020-07-31 15:44:38 +0900

    Updates

---
+++ precache-manifest.8f9bac552c8fd67c73bcb69d38f5458a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b5221a5a7c3b54f86cf359125ac147",
+    "url": "/2020_movie/index.html"
+  },
+  {
+    "revision": "cb8cfc71ec3667250008",
+    "url": "/2020_movie/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "102d9b8a664664120799",
+    "url": "/2020_movie/static/js/2.928ccd5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020_movie/static/js/2.928ccd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8cfc71ec3667250008",
+    "url": "/2020_movie/static/js/main.374c83f7.chunk.js"
+  },
+  {
+    "revision": "4fa282fd20d4abb9eab2",
+    "url": "/2020_movie/static/js/runtime-main.f35e1c6a.js"
+  }
+]);
